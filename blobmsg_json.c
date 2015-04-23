11de358d3a27f3336c2c2e9cebedda41418fa755
Karthik Kumar Viswanathan <kk@geog.co>
2015-04-23 17:25:14 +0530

    Const Fixes.

--- blobmsg_json.c
+++ blobmsg_json.c
@@ -97,7 +97,8 @@ out:
 
 bool blobmsg_add_json_from_file(struct blob_buf *b, const char *file)
 {
-	return __blobmsg_add_json(b, json_object_from_file(file));
+	char *cfile = (char *)file;
+	return __blobmsg_add_json(b, json_object_from_file(cfile));
 }
 
 bool blobmsg_add_json_from_string(struct blob_buf *b, const char *str)
